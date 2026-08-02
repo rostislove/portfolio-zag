4b462e0e73b513c9b69d321b92939f4cdbc4d3e0
Rostislove <rostislove.rh14@gmail.com>
2026-08-02 14:11:47 +0300

    remove tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react()],
   base: 'rostislove.github.io',
 })
