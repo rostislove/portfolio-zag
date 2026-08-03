3e71fac54e7bc92d29f7dea5c84c8d943323cb38
Rostislove <rostislove.rh14@gmail.com>
2026-08-03 10:18:35 +0300

    Upd tailwind package

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from 'node_modules/@tailwindcss/vite/dist/index.mjs'
 
 // https://vite.dev/config/
 export default defineConfig({
