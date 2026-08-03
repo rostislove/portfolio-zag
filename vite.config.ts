f29f4a064b2c269267e1a90ba7ff0b5945233f6e
Rostislove <rostislove.rh14@gmail.com>
2026-08-03 17:53:51 +0300

    remove base github io

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: 'rostislove.github.io',
+  plugins: [react(),tailwindcss(),],
 })
